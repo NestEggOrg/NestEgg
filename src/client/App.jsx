f3e033842c3c7375c645bca93d75f8242c8893e3
Howard Sun <komugi@gmail.com>
2024-04-22 22:53:59 -0400

    Working tailwindcss

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const App = () => (
-  <div id='app' className='flex justify-center items-center h-screen'>
-    <h1 className='text-4xl text-blue-200 bg-gray-800 p-4 rounded-lg'>
-      Hello World
-    </h1>
-  </div>
-);
+const App = () => {
+  return (
+    <div id='app' className='flex justify-center items-center h-screen'>
+      <h1 className='text-4xl text-blue-200 bg-gray-800 p-4 rounded-lg'>
+        Hello World
+      </h1>
+    </div>
+  );
+};
 
 export default App;
