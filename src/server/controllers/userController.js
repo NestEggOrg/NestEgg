0e751a92c4b428a982bce02697f92665ce227138
Reem <abiammar.reem@gmail.com>
2024-04-23 12:37:42 -0300

    created user controller

---
+++ src/server/controllers/userController.js
@@ -0,0 +1,13 @@
+
+const userController = {}; 
+
+/*
+create user - when user signs up with a unique username and valid password
+*/
+userController.createUser = (req, res, next) =>{
+
+
+
+}
+
+module.exports = userController; 
