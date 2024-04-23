d85304e29b4382496ea05d2cca91d05cb55829ec
Reem <abiammar.reem@gmail.com>
2024-04-23 12:39:45 -0300

    committing after merging with server branch

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -4,7 +4,13 @@ const userController = {};
 /*
 create user - when user signs up with a unique username and valid password
 */
-userController.createUser = (req, res, next) =>{
+userController.createUser = async (req, res, next) =>{
+    // deconstruct username and password from req.body 
+    const {username, password} = req.body; 
+    // check if username already exists and if it does, send 'username already exists' to front-end
+    const params = [username]
+    const usernameQuery = "SELECT * from users WHERE username = $1"
+    const result = await db.query(usernameQuery, params)
 
 
 
