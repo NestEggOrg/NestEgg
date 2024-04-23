4ea65a5bb0b18a4cc6a0104bd254342948ef9b5d
Michael Underbrink <michaelunderbrink@Michaels-MBP.lan>
2024-04-23 08:40:03 -0500

    addition of newexpense and user info components

---
+++ src/client/Components/UserInformation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserInformation = () => {
+  return (
+    <div>UserInformation
+      <button></button>
+    </div>
+  )
+  ;
+};
+
+export default UserInformation;
