04b1b90c1d48d23ba533c304306f5bb8a2532bc1
Reem <abiammar.reem@gmail.com>
2024-04-23 19:50:21 -0300

    tested main

--- src/server/controllers/sessionController.js
+++ src/server/controllers/sessionController.js
@@ -1,5 +1,11 @@
 const sessionController = {
-  startSession(){
+/* when user signs up or logs in successfully create cookie corresponding to user and save it in DB
+Other controllers will check if this cookie is valid to verify user is logged in 
+cookie will have a life of 30 min 
+*/ 
+  startSession: async (req, res, next)=> {
+
+
     
   },
   
