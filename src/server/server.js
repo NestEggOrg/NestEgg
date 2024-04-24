9f058375b1024f98714f490f28cfc184041bf6f8
Reem <abiammar.reem@gmail.com>
2024-04-23 21:49:44 -0300

    cookie showing on postman

--- src/server/server.js
+++ src/server/server.js
@@ -1,46 +1,52 @@
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const app = express();
 const path = require('path');
 const PORT = 3000;
 const userController = require('./controllers/userController');
+const sessionController = require('./controllers/sessionController')
+
 
 
 const db = require('./models/dbModels');
 
 //parse incoming JSON and form data
+app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded());
 
-// Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, '../../dist')));
 
 // Routes
 
-app.get('/example', async (req, res) => {
-  try {
-    const result = await db.query('SELECT * FROM categories');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
+// app.get('/example', async (req, res) => {
+//   try {
+//     const result = await db.query('SELECT * FROM categories');
+//     res.json(result.rows);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send('Internal Server Error');
+//   }
+// });
 
 
 // adding route for /signup that directs to userController and sessionController
-app.post('/signup', userController.createUser, (req, res) => {
+app.post('/signup', userController.createUser, sessionController.startSession, (req, res) => {
   res.status(200).json(res.locals.signUpMessage); 
 
 })
 
 // adding route for /signin that directs to userController and sessionController
-app.post('/signin', userController.verifyUser, (req, res) => {
+app.post('/signin', userController.verifyUser, sessionController.startSession, (req, res) => {
+  console.log('cookie', req.cookie)
   res.status(200).json(res.locals.signInMessage); 
 
 })
 
+
+// Serve static files from the 'dist' directory
+app.use(express.static(path.join(__dirname, '../../dist')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
+  return res.sendFile(path.join(__dirname, '../../dist', 'index.html'));
 });
 
 
@@ -60,7 +66,7 @@ app.use((err, req, res, next) => {
   };
   // Object.assign to overwrite defaultErr with properties from err
   const errorObj = Object.assign(defaultErr, err);
-  console.log('We are in the global error handler: ', errorObj.log);
+  console.log('We are in the global error handler: ',err, errorObj.log);
   // respond using errObj status
   return res.status(errorObj.status).send(errorObj.message);
 });
